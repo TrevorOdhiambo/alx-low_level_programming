7742bf0e69085a508e7f7c86fbafb075e82880a5
TrevorOdhiambo <trevodhiambo@gmail.com>
2023-02-17 13:55:03 -0800

    combine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+		if (i == 9)
+		{
+			putchar('$');
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
