f5fb63a1ea58451c46179338cdb5a2c90f5a0251
TrevorOdhiambo <trevodhiambo@gmail.com>
2023-02-21 12:55:34 -0800

    last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,13 @@ int print_last_digit(int c)
 {
 	int num;
 
-	if (c < 0)
-	{
-		c = -c;
-	}
 	num = c % 10;
+
+	if (num < 0)
+	{
+		num = num * -1;
+	}
 	_putchar(num + '0');
+
 	return (num);
 }
