7a47c0262d59d672244235abf9dd0214af582dfd
TrevorOdhiambo <trevodhiambo@gmail.com>
2023-02-21 11:57:03 -0800

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * _isalpha - check if input is in the alphabet
  * @c: takes a charcter c
@@ -11,19 +10,3 @@ int _isalpha(int c)
 	return ((c >= 'a' && c <= 'z') && (c >= 'A' && c >= 'Z'));
 }
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	int r;
-
-	r = _isalpha('o');
-	_putchar(r + '0');
-	_putchar('\n');
-
-	return (0);
-}
