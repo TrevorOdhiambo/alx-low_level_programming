e513cc1f41e2ebc8a4d1868a2b5f71ca3eab8f3f
TrevorOdhiambo <trevodhiambo@gmail.com>
2023-02-23 17:25:19 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz\n");
 		} else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz\n");
 		} else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz\n");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d\n", i);
 		}
 	}
 	printf("\n");
